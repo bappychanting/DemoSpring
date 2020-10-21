a7db344723a9fac569afa000240880777a3834b9
Mahadi Hasan <bappy@pop-os.localdomain>
2020-10-21 16:01:10 +0600

    transaction done

--- src/main/java/com/bappy/npspring5tutorial/NpSpring5TutorialApplication.java
+++ src/main/java/com/bappy/npspring5tutorial/NpSpring5TutorialApplication.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages={"com.bappy"})
+@EnableAutoConfiguration
+@EnableTransactionManagement
 public class NpSpring5TutorialApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(NpSpring5TutorialApplication.class);
--- src/main/java/com/bappy/npspring5tutorial/services/UserServiceImpl.java
+++ src/main/java/com/bappy/npspring5tutorial/services/UserServiceImpl.java
@@ -2,12 +2,15 @@ package com.bappy.npspring5tutorial.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.annotation.Propagation;
 
 import com.bappy.npspring5tutorial.dto.SignupForm;
 import com.bappy.npspring5tutorial.entities.User;
 import com.bappy.npspring5tutorial.repositories.UserRepository;
 
 @Service
+@Transactional(propagation=Propagation.SUPPORTS, readOnly=true)
 public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepository;
@@ -18,6 +21,7 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	@Override
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void signup(SignupForm signupForm) {
 		User user = new User();
 		user.setEmail(signupForm.getEmail());
